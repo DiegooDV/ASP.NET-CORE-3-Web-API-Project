76b717527c232bb577e2658e4c9303cd3fbfb133
Diego De la Vega <diego_dv7@outlook.com>
2020-10-06 23:47:09 -0500

    Added Sql connection string | Created migration files | Added Repository base, Company and Employee interfaces and repositories

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using Contracts;
+using Entities;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -29,6 +32,12 @@ namespace CompanyEmployees.Extensions
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddScoped<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => 
+            b.MigrationsAssembly("CompanyEmployees")));
 
 
     }  
---
+++ Repository/EmployeeRepositoryClass.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    class EmployeeRepositoryClass : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepositoryClass (RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
